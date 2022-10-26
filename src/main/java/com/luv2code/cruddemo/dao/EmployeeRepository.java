19c77a90faea262c2ec0ab80018dd0aea1993be0
admin <admin@admins-imac.gateway>
2022-10-26 21:26:33 +1100

    5th commit - Spring Data REST - Create a REST API
    Use Spring Data REST to replace the rest controller and service layer.

--- src/main/java/com/luv2code/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/cruddemo/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.luv2code.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.luv2code.cruddemo.entity.Employee;
 
+@RepositoryRestResource(path = "members")	// we specify path for Employee entity
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> // Employee is entity type, 
 																				// Integer is Primary key type
 {
